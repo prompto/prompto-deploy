b4a871898b195d5de8f76c0f373f90a262c9a1b2
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-03-19 20:56:48 +0800

    updated speed script

--- speed-scripts/update-java-core-only.sh
+++ speed-scripts/update-java-core-only.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #update latest Core
-cd ~/Development/prompto/prompto-platform/prompto-java/Core
+cd ~/Development/prompto/prompto-platform/prompto-java/Core || exit
 mvn clean install -DskipTests=true
-scp -i ~/Development/prompto/prompto-keys/aws/prompto-admin.pem ~/.m2/repository/org/prompto/Core/0.0.1-SNAPSHOT/Core-0.0.1-SNAPSHOT.jar root@eric.prompto.cloud:/Core-0.0.1-SNAPSHOT.jar
+scp -i ~/Documents/Technical/Certificates/prompto-keys/aws/us-east-1/prompto-admin.pem ~/.m2/repository/org/prompto/Core/0.0.1-SNAPSHOT/Core-0.0.1-SNAPSHOT.jar root@eric.prompto.cloud:/Core-0.0.1-SNAPSHOT.jar
 

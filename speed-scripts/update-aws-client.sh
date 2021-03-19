2e8e487185c1cafb67c638bc467641edbcbac284
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-03-19 14:13:03 +0800

    update speed script

--- speed-scripts/update-aws-client.sh
+++ speed-scripts/update-aws-client.sh
@@ -3,4 +3,4 @@
 #copy latest AwsClient
 cd ~/Development/prompto/prompto-platform/AwsClient
 mvn clean install -DskipTests=true
-scp -i ~/Development/prompto/prompto-keys/aws/prompto-admin.pem ~/.m2/repository/org/prompto/AwsClient/0.0.1-SNAPSHOT/AwsClient-0.0.1-SNAPSHOT.jar root@eric.prompto.cloud:/AwsClient/AwsClient-0.0.1-SNAPSHOT.jar
+scp -i ~/Documents/Technical/Certificates/prompto-keys/aws/us-east-1/prompto-admin.pem ~/.m2/repository/org/prompto/AwsClient/0.0.1-SNAPSHOT/AwsClient-0.0.1-SNAPSHOT.jar root@eric.prompto.cloud:/AwsClient/AwsClient-0.0.1-SNAPSHOT.jar

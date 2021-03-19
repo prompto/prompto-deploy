31fdf8e0a7713d760494c0442c157a4eeb2e7c95
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-03-19 21:15:19 +0800

    configure factory logs

--- aws/prompto-factory/deploy.sh
+++ aws/prompto-factory/deploy.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-export PRODUCT_VERSION=$version$
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+export INSTANCE_ID
+PRODUCT_VERSION=$version$
+export PRODUCT_VERSION
+# connect logs file system
+AWS_REGION=$(curl http://169.254.169.254/latest/meta-data/placement/region)
+LOGS_FILESYSTEM_URL=fs-666f46d3.efs.${AWS_REGION}.amazonaws.com
+mkdir -p /mnt/efs/logs
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${LOGS_FILESYSTEM_URL}:/ /mnt/efs/logs
+mkdir -p /mnt/efs/logs/eric/${INSTANCE_ID}
+ln -s /mnt/efs/logs/eric/${INSTANCE_ID} /logs
 # download resources
-export DEPLOYER_ROOT=https://raw.githubusercontent.com/prompto/prompto-deploy/master
+DEPLOYER_ROOT=https://raw.githubusercontent.com/prompto/prompto-deploy/master
+export DEPLOYER_ROOT
 mkdir ~/.m2
 curl $DEPLOYER_ROOT/maven/nexus-settings.xml -o ~/.m2/settings.xml
 curl $DEPLOYER_ROOT/aws/deploy.py -o deploy.py
@@ -9,7 +20,6 @@ curl $DEPLOYER_ROOT/aws/prompto-factory/config.yml -o config.yml
 curl $DEPLOYER_ROOT/certificates/prompto-cloud/keystore.jks -o keystore.jks
 curl $DEPLOYER_ROOT/certificates/prompto-cloud/truststore.jks -o truststore.jks
 # run deployer
-python deploy.py CodeFactory $PRODUCT_VERSION latest
+python deploy.py CodeFactory ${PRODUCT_VERSION} latest
 # notify deployer service
-export INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-curl --insecure  "https://deploy.prompto.cloud/ec2/installed?instanceId=$INSTANCE_ID&component=prompto-factory&addressId=eipalloc-1f9ad42d"
+curl --insecure  "https://deploy.prompto.cloud/ec2/installed?instanceId=${INSTANCE_ID}&component=prompto-factory&addressId=eipalloc-1f9ad42d"

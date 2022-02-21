944415a62e6b87ee463d519c9c3a56184bd1e5be
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2022-02-22 00:14:36 +0100

    latest scripts for linux 2

--- aws/prepare-ami-aws-linux.sh
+++ aws/prepare-ami-aws-linux.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 sudo su root
-# wget
-yum install -y wget
+yum -y update
+yum install -y amazon-efs-utils
+yum install -y nfs-utils
+cd /tmp
+# java
+wget https://download.oracle.com/java/17/archive/jdk-17.0.2_linux-x64_bin.rpm
+yum -y localinstall jdk-17.0.2_linux-x64_bin.rpm
+echo export JAVA_HOME=/usr/java/jdk-17.0.2/ >> /etc/environment
+# maven
+wget https://www-eu.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+tar xf /tmp/apache-maven-*.tar.gz -C /opt
+ln -s /opt/apache-maven-3.6.3 /opt/maven
+echo "export JAVA_HOME=/usr/java/jdk-17.0.2/" >> /etc/profile.d/maven.sh
+echo "export M2_HOME=/opt/maven" >> /etc/profile.d/maven.sh
+echo "export MAVEN_HOME=/opt/maven" >> /etc/profile.d/maven.sh
+echo "export PATH=\${M2_HOME}/bin:\${PATH}" >> /etc/profile.d/maven.sh
+chmod +x /etc/profile.d/maven.sh
+source /etc/profile.d/maven.sh
 # mongo
 echo "[mongodb-org-4.4]" >> /etc/yum.repos.d/mongodb-org-4.4.repo
 echo "name=MongoDB Repository" >> /etc/yum.repos.d/mongodb-org-4.4.repo
@@ -10,8 +26,3 @@ echo "gpgcheck=1" >> /etc/yum.repos.d/mongodb-org-4.4.repo
 echo "enabled=1" >> /etc/yum.repos.d/mongodb-org-4.4.repo
 echo "gpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc" >> /etc/yum.repos.d/mongodb-org-4.4.repo
 yum install -y mongodb-org-tools
-# maven
-yum install -y maven2
-# java
-// corretto installed by maven2
-echo export JAVA_HOME=/usr/java/jdk-17.0.2/ >> /etc/environment
--- aws/prompto-factory/deploy.sh
+++ aws/prompto-factory/deploy.sh
@@ -7,7 +7,7 @@ export PRODUCT_VERSION
 AWS_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region)
 LOGS_FILESYSTEM_URL=fs-666f46d3.efs.${AWS_REGION}.amazonaws.com
 mkdir -p /mnt/efs/logs
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${LOGS_FILESYSTEM_URL}:/ /mnt/efs/logs
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${LOGS_FILESYSTEM_URL}:/ /mnt/efs/logs
 mkdir -p /mnt/efs/logs/eric/${INSTANCE_ID}
 ln -s /mnt/efs/logs/eric/${INSTANCE_ID} /logs
 # download resources
@@ -15,11 +15,11 @@ DEPLOYER_ROOT=https://raw.githubusercontent.com/prompto/prompto-deploy/master
 export DEPLOYER_ROOT
 mkdir ~/.m2
 curl $DEPLOYER_ROOT/maven/nexus-settings.xml -o ~/.m2/settings.xml
-curl $DEPLOYER_ROOT/aws/deploy.py -o deploy.py
-curl $DEPLOYER_ROOT/aws/prompto-factory/config.yml -o config.yml
-curl $DEPLOYER_ROOT/certificates/prompto-cloud/keystore.jks -o keystore.jks
-curl $DEPLOYER_ROOT/certificates/prompto-cloud/truststore.jks -o truststore.jks
+curl $DEPLOYER_ROOT/aws/deploy.py -o /deploy.py
+curl $DEPLOYER_ROOT/aws/prompto-factory/config.yml -o /config.yml
+curl $DEPLOYER_ROOT/certificates/prompto-cloud/keystore.jks -o /keystore.jks
+curl $DEPLOYER_ROOT/certificates/prompto-cloud/truststore.jks -o /truststore.jks
 # run deployer
-python deploy.py CodeFactory ${PRODUCT_VERSION} latest
+python /deploy.py CodeFactory ${PRODUCT_VERSION} latest
 # notify deployer service
 curl --insecure  "https://deploy.prompto.cloud/ec2/installed?instanceId=${INSTANCE_ID}&component=prompto-factory&addressId=eipalloc-1f9ad42d"

541b0d4d0ad6220b105580eca4668b075705cc7d
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2022-03-03 02:44:01 +0100

    fix ami script

--- aws/prepare-ami.py
+++ aws/prepare-ami.py
@@ -4,7 +4,7 @@ from deploy import *
 if __name__ == '__main__':
     version = sys.argv[1]
     if version == "latest":
-        version = fetchLatestAwsVersion()
+        version = fetchLatestPromptoVersion()
     sudo = ""
     install_tools(sudo)
     install_libs(sudo)
